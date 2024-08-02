312c6c1da8eeb90ab8f032f37c7abc7c826f2acf
KritanDotel69 <dotelkritan8@gmail.com>
2024-08-02 18:57:46 +0545

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
-import About from './pages/About'
+
 import NotFound from './pages/NotFound'
 import RootLayOut from './components/RootLayOut'
-import DetailPage from './pages/DetailPage'
+
 
 
 
@@ -16,9 +16,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<RootLayOut />} >
           <Route index element={<HomePage />} />
-          <Route path='detail/:id' element={<DetailPage />} />
-
-          <Route path='about-page' element={<About />} />
+        
           <Route path='*' element={<NotFound />} />
 
         </Route>
